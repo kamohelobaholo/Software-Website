5acd7f44ce188c09630fe4978f778deea221e3ac
Dari <kamohelobaholo076@gmail.com>
2025-05-23 00:10:11 +0200

    add dark/light mode toggle and upload functionality

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let title = document.getElementById("title");
+console.log(title);
+let image = document.getElementById("My-Image");
+console.log(image);
+let body = document.getElementsByTagName()
+
+
+
+
+function someFunction(){
+    
+    let otherImage = image;
+    body.appendChild(otherImage);
+
+    //console.log(title.innerText);
+}
+
