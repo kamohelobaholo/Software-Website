844f78bf75fe09716ccad7b09789e49e5e7204c4
Dari <kamohelobaholo076@gmail.com>
2025-06-27 18:44:09 +0200

    All the work

--- Calculator/JavaCode.js
+++ Calculator/JavaCode.js
@@ -1,18 +1,31 @@
 let display = document.getElementById('display');
-funtion appendValue(value){
-    display.value += value;
+let historydiv = document.getelementarybyId('history');
+let history = [];
+function append(char){
+    if (display.innerText === '0 '|| display.innerText === 'Error'){
+        display.innerText = char;
+    }else{
+        display.innerText += char;
+    }
 }
 function clearDisplay() {
-    display.value = '';
+    display.value = '0';
 }
 function deletelast() {
     display.value = display.value.slice((0,-1));
 }
 function calculate(){
     try {
-        display.value = eval(display.value);
+        let results = eval(display.innerText.replace('///g', '/').replace('/*/g', '*'));
+            history.pushState(display.innertext + "="+ result);
+            historydiv.innerHTML = history.slice(-5).reverse().join('<br>');
+
+       // display.value = eval(display.value);
     }
     catch{
-        display.value = 'error';
+        display.innerText = 'error';
     }
+}
+function toggleMode(){
+    document.body.classList.toggle('dark');
 }
